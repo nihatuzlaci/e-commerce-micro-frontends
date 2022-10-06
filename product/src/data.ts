c028c292482d60afdfeae5627610922c894a08a7
Nihat <senius.33@gmail.com>
2022-10-07 01:40:22 +0300

    add static data

---
+++ product/src/data.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Georgia Dress // Aurora Red",
+    price: 220,
+    imageURL: "https://i.imgur.com/OXyGNyA.jpeg",
+  },
+  {
+    id: 2,
+    name: "Jackson Tee",
+    price: 168,
+    imageURL: "https://i.imgur.com/GpcOP08.jpeg",
+  },
+  {
+    id: 3,
+    name: "Sutton Sweater // Black",
+    price: 260,
+    imageURL: "https://i.imgur.com/oTbXByj.jpeg",
+  },
+  {
+    id: 4,
+    name: "Runa Skirt // Black",
+    price: 350,
+    imageURL: "https://i.imgur.com/rP94WnB.jpeg",
+  },
+  {
+    id: 5,
+    name: "Marsden 2.0 Crop // Evergreen",
+    price: 150,
+    imageURL: "https://i.imgur.com/L5ebWyi.jpeg",
+  },
+  {
+    id: 6,
+    name: "Late Checkout Cap //Clay",
+    price: 70,
+    imageURL: "https://i.imgur.com/0KeK1Cd.jpeg",
+  },
+];
